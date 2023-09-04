77de72d1ebced458a527ecf98e3f71a671a0253a
Lynda Chiwetelu <lyndachiwe@gmail.com>
2023-09-04 17:19:19 +0200

    Initial Commit

---
+++ NavigationStack/NavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackApp.swift
+//  NavigationStack
+//
+//  Created by Lynda Chiwetelu on 04.09.23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
